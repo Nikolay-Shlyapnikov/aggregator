68d8190fd98b6b0c2d2c088be7b04601f58faf8f
Nikolay <shlyapnikov.nik@mail.ru>
2025-12-08 20:03:01 +0300

    Добавить header и цветные иконки

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 import { InSearch } from '../features/search/components/SearchProcess/InSearch'
 import { UploadPost } from '../features/user/uploadPost/UploadPost'
+import { Header } from '../features/header/Header'
+import styles from '../app/styles/styles.scss'
 
 export const MainPage = () => {
   return (
     <>
-      <div>
+      <Header />
+      <main className={styles.container}>
         <InSearch />
         <UploadPost />
-      </div>
+      </main>
     </>
   )
 }
