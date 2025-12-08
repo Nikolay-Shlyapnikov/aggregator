cbb300c7326821875f414b573e6adab980441802
Nikolay <shlyapnikov.nik@mail.ru>
2025-12-08 19:36:56 +0300

    Добавить searchProcess

---
+++ src/features/post/store/types.ts
@@ -0,0 +1,7 @@
+export type Post = {
+    id: string
+    name: string
+    description: string
+    'created-at': string
+    'tags': string[]
+}
---
+++ src/features/search/store/types.ts
@@ -0,0 +1,5 @@
+import {Post} from "../../post/store/types";
+
+export type SearchSlice = {
+    posts: Post[]
+}
